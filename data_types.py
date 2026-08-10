c8ec20df47ca981dce70b48678eafd69a7083534
@dementeva.ve <dementeva.ve@medsigroup.ru>
2026-08-10 20:30:11 +0300

    изучение типов данных

---
+++ data_types.py
@@ -0,0 +1,81 @@
+# numm = int(input())  # преобразование считанной строки в целое число
+# nn = int('12345')  # преобразование строки в целое число
+#
+# a = 13
+# b = 7
+#
+# total = a + b
+# diff = a - b
+# prod = a * b
+# div1 = a / b
+# div2 = a // b
+# mod = a % b
+# exp = a ** b
+#
+# print(a, '+', b, '=', total)
+# print(a, '-', b, '=', diff)
+# print(a, '*', b, '=', prod)
+# print(a, '/', b, '=', div1)
+# print(a, '//', b, '=', div2)
+# print(a, '%', b, '=', mod)
+# print(a, '**', b, '=', exp)
+#
+# atom = 10 ** 80  # количество атомов во вселенной
+# print('Количество атомов =', atom)
+#
+# num1 = 25_000_000
+# num2 = 25000000
+#
+# print(num1)
+# print(num2)
+#
+# num = float(input())  # преобразование считанной строки в число с плавающей точкой
+# n = float('1.2345')  # преобразование строки к числу с плавающей точкой
+# a = 13.5
+# b = 2.0
+#
+# total = a + b
+# diff = a - b
+# prod = a * b
+# div = a / b
+# exp = a ** b
+#
+# print(a, '+', b, '=', total)
+# print(a, '-', b, '=', diff)
+# print(a, '*', b, '=', prod)
+# print(a, '/', b, '=', div)
+# print(a, '**', b, '=', exp)
+
+# a = 13.5
+# b = 2.0
+# int_div = a // b
+# mod = a % b
+#
+# print(a, '//', b, '=', int_div)
+# print(a, '%', b, '=', mod)
+#
+# # преобразование чисел с точкой в целое производится с округлением в сторону нуля int(1.7) = 1, int(-1.7) = -1
+# num1 = 17.89
+# num2 = -13.56
+# num3 = int(num1)
+# num4 = int(num2)
+#
+# print(num3)
+# print(num4)
+
+num1 = int(1.5)
+num2 = int(6)
+
+print(num1 + num2)
+
+a = max(3, 8, -3, 12, 9)
+b = min(3, 8.5, -3.2, 12, 9)
+c = max(3.14, 2.17, 9.8)
+print(a)
+print(b)
+print(c)
+
+print(abs(1 - 4))
+print(abs(-7))
+print(abs(0))
+print(abs(-17.67))
--- exam3.py
+++ exam3.py
@@ -1,25 +1,6 @@
-a = int(input())
+p1 = int(input())
+p2 = int(input())
+q1 = int(input())
+q2 = int(input())
 
-r = "красный"
-b = "черный"
-
-if a < 0 or a > 36:
-    print("ошибка ввода")
-elif a == 0:
-    print("зеленый")
-elif (a >= 1 and a <= 10) and a % 2 == 0:
-    print(b)
-elif (a >= 1 and a <= 10) and a % 2 != 0:
-    print(r)
-elif (a >= 11 and a <= 18) and a % 2 == 0:
-    print(r)
-elif (a >= 11 and a <= 18) and a % 2 != 0:
-    print(b)
-elif (a >= 19 and a <= 28) and a % 2 == 0:
-    print(b)
-elif (a >= 19 and a <= 28) and a % 2 != 0:
-    print(r)
-elif (a >= 29 and a <= 36) and a % 2 == 0:
-    print(r)
-elif (a >= 29 and a <= 36) and a % 2 != 0:
-    print(b)
+print(abs(p1 - q1) + abs(p2 - q2))
